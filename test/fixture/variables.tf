11f66934a64d067fcb7f2327d894bcb06408ece8
craig.holmes <craig.holmes@lvs1.com>
2022-06-10 23:05:14 +0000

    feat: updated test

--- test/fixture/variables.tf
+++ test/fixture/variables.tf
@@ -1,27 +1,49 @@
-variable "la_workspace_name" {
-  type = string
-}
-
-variable "automation_account_name" {
-  type = string
-}
-
-variable "resource_group_name" {
-  type = string
-}
-
 variable "location" {
-  type = string
+  type        = string
+  description = "Azure location where resources should be deployed."
+  default     = "westus"
+}
+
+variable "sku" {
+  type        = string
+  description = "Specified the Sku of the Log Analytics Workspace."
+  default     = "PerGB2018"
+}
+
+variable "la_retention_in_days" {
+  type        = number
+  description = "The workspace data retetion in days. Possible values range between 30 and 730."
+  default     = 30
 }
 
 variable "security_center_subscription" {
-  type = list(string)
+  description = "List of subscriptions this log analytics should collect data for. Does not work on free subscription."
+  type        = list(string)
+  default     = []
 }
 
 variable "solutions" {
-  type = list(any)
+  type        = list(any)
+  description = "A list of solution names to add to the workspace."
+
+  default = [
+    "AgentHealthAssessment",
+    "AntiMalware",
+    "AzureActivity",
+    "ChangeTracking",
+    "Security",
+    "SecurityInsights",
+    "ServiceMap",
+    "SQLAssessment",
+    "Updates",
+    "VMInsights"
+  ]
 }
 
 variable "tags" {
-  type = map(string)
+  description = "Tags to apply to all resources created."
+  type        = map(string)
+  default = {
+    owner = "terratest"
+  }
 }
